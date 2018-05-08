855a9bcf318ab632a18980d6a2b2c269f7d69fd6
Rene <tak_esouza@uolinc.com>
2018-05-08 17:31:43 -0300

    [OfflineApp] - Criando e/ou consultando o cache  e inserindo assets nele

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,4 +1,21 @@
-self.addEventListener('fetch', function(event){
+self.addEventListener('install', function(event){
+    var urlsToCache = [
+        '/',
+        'js/main.js',
+        'css/main.css',
+        'imgs/icon.png',
+        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+    ];
+    event.waitUntil(
+        caches.open('wittr-static-v1')
+        .then(function(cache){
+            return cache.addAll(urlsToCache);
+        })
+    )
+});
+
+self.addEventListener('fetch', function(event){ //intercepta todas requisições
     event.respondWith(
         fetch(event.request)
         .then(function(response){
